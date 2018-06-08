1a607cd9e16eabe7aad3006d6cce1472aa77399b
Viktoria Kozopas <kozopasv@gmail.com>
2018-06-08 23:36:57 +0300

    add update which gives +1 ppc
    ppc - point per click

--- Kyiv_Mohyla_Clickacademy/Assets/Scripts/TableClicker.cs
+++ Kyiv_Mohyla_Clickacademy/Assets/Scripts/TableClicker.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class TableClicker : MonoBehaviour {
 
     public UnityEngine.UI.Text pointsDisplay;
+    public UnityEngine.UI.Text pointsPerClickDisplay;
     public int points = 0;
     public int pointsPerClick = 1;
 
     void Update()
     {
         pointsDisplay.text = "Points: " + points;
+        pointsPerClickDisplay.text = "P/C: " + pointsPerClick;
     }
 
     public void Clicked()
---
+++ Kyiv_Mohyla_Clickacademy/Assets/Scripts/UpgradeManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeManager : MonoBehaviour {
+
+    public TableClicker tableClicker;
+    public UnityEngine.UI.Text upgradeInfo;
+
+    public string upgradeName;
+    public int upgradeCost;
+    public int upgradePPS;
+
+	void Update () {
+        upgradeInfo.text = upgradeName + '\n' + upgradeCost + " points +" + upgradePPS + " pps";
+	}
+
+    public void BoughtUpdate()
+    {
+        // when we buy update subtract cost and add ppc
+        if(tableClicker.points >= upgradeCost)
+        {
+            tableClicker.points -= upgradeCost;
+            tableClicker.pointsPerClick += upgradePPS;
+        }
+    }
+}
