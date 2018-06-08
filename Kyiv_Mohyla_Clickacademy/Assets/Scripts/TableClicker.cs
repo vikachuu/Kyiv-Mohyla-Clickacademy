de984ddb677b09b08e27eb36ca8cbdf0221659ef
Viktoria Kozopas <kozopasv@gmail.com>
2018-06-08 21:48:07 +0300

    Add table button and counter for every click

---
+++ Kyiv_Mohyla_Clickacademy/Assets/Scripts/TableClicker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableClicker : MonoBehaviour {
+
+    public UnityEngine.UI.Text pointsDisplay;
+    public int points = 0;
+    public int pointsPerClick = 1;
+
+    void Update()
+    {
+        pointsDisplay.text = "Points: " + points;
+    }
+
+    public void Clicked()
+    {
+        points += pointsPerClick;
+    }
+
+}
