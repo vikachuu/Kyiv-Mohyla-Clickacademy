899541aec573248131b1494a002e98e89f26ca50
Viktoria Kozopas <kozopasv@gmail.com>
2018-06-24 01:23:28 +0300

    hunger and sleep bars add

---
+++ Kyiv_Mohyla_Clickacademy/Assets/Scripts/SliderScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderScript : MonoBehaviour {
+    public Slider hungerSlider;
+    private float currentHunger;
+
+	// Use this for initialization
+	void Start () {
+        currentHunger = hungerSlider.maxValue;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentHunger -= Time.deltaTime;
+        hungerSlider.value = currentHunger;
+        if(hungerSlider.value <= 0)
+        {
+            //TODO: if player is _
+            currentHunger = hungerSlider.maxValue;
+            hungerSlider.value = hungerSlider.maxValue;
+        }
+	}
+}
