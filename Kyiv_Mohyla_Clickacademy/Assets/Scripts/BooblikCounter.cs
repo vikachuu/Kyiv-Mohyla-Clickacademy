6f1485399d860d68a21d2de8193aec6af0218d50
DenysVasylenko <32224385+flintojkee@users.noreply.github.com>
2018-06-25 18:18:22 +0300

    +1 animation

---
+++ Kyiv_Mohyla_Clickacademy/Assets/Scripts/BooblikCounter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BooblikCounter : MonoBehaviour {
+	// Use this for initialization
+	public int id = 1;
+	void Start () {
+
+	}
+	// Update is called once per frame
+	void Update () {
+	}
+	public void increaseId(){
+		id++;
+	}
+	public void decreaseId(){
+		id--;
+	}
+}
---
+++ Kyiv_Mohyla_Clickacademy/Assets/Scripts/destroyBooblik.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyBooblik : MonoBehaviour {
+	// Use this for initialization
+	public BooblikCounter boobliks;
+	private int ID;
+	void Start () {
+		ID = boobliks.id;
+		boobliks.increaseId ();
+	}
+	void DestroyObjectDelayed()
+	{
+		// Kills the game object in 5 seconds after loading the object
+		Destroy(gameObject, 2);
+	}
+	// Update is called once per frame
+	void Update () {
+		if (ID > 1) {
+			DestroyObjectDelayed ();
+		}
+	}
+}
